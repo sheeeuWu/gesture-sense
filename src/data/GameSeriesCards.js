5918474c487c752673e758bb212197a3aa7213eb
SaifaliCodes <saifali@redefinealgo.com>
2025-01-27 16:59:21 +0530

    added mediapipe Hand landmarks detection

---
+++ src/data/GameSeriesCards.js
@@ -0,0 +1,66 @@
+export const GameSeriesCards = [
+  {
+    imageUrl:
+      "https://ai.google.dev/static/mediapipe/images/solutions/examples/hand_landmark.png",
+    title: "Hand Gestures Game",
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus potenti euismod habitasse taciti pretium eleifend.",
+    page: "/hand-gestures-game",
+  },
+  {
+    imageUrl:
+      "https://ai.google.dev/static/edge/mediapipe/images/solutions/examples/pose_detector_480.png",
+    title: "Body Pose Game",
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus potenti euismod habitasse taciti pretium eleifend.",
+    page: "/body-pose-game",
+  },
+  // {
+  //   imageUrl:
+  //     "https://ai.google.dev/static/mediapipe/images/solutions/examples/hand_landmark.png",
+  //   title: "Hand Gestures Game",
+  //   description:
+  //     "Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus potenti euismod habitasse taciti pretium eleifend.",
+  //   page: "/hand-gestures-game",
+  // },
+  // {
+  //   imageUrl:
+  //     "https://ai.google.dev/static/edge/mediapipe/images/solutions/examples/pose_detector_480.png",
+  //   title: "Body Pose Game",
+  //   description:
+  //     "Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus potenti euismod habitasse taciti pretium eleifend.",
+  //   page: "/body-pose-game",
+  // },
+  // {
+  //   imageUrl:
+  //     "https://ai.google.dev/static/mediapipe/images/solutions/examples/hand_landmark.png",
+  //   title: "Hand Gestures Game",
+  //   description:
+  //     "Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus potenti euismod habitasse taciti pretium eleifend.",
+  //   page: "/hand-gestures-game",
+  // },
+  // {
+  //   imageUrl:
+  //     "https://ai.google.dev/static/edge/mediapipe/images/solutions/examples/pose_detector_480.png",
+  //   title: "Body Pose Game",
+  //   description:
+  //     "Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus potenti euismod habitasse taciti pretium eleifend.",
+  //   page: "/body-pose-game",
+  // },
+  // {
+  //   imageUrl:
+  //     "https://ai.google.dev/static/mediapipe/images/solutions/examples/hand_landmark.png",
+  //   title: "Hand Gestures Game",
+  //   description:
+  //     "Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus potenti euismod habitasse taciti pretium eleifend.",
+  //   page: "/hand-gestures-game",
+  // },
+  // {
+  //   imageUrl:
+  //     "https://ai.google.dev/static/edge/mediapipe/images/solutions/examples/pose_detector_480.png",
+  //   title: "Body Pose Game",
+  //   description:
+  //     "Lorem ipsum odor amet, consectetuer adipiscing elit. Lacus potenti euismod habitasse taciti pretium eleifend.",
+  //   page: "/body-pose-game",
+  // },
+];
